d103ae357cc20d4f6224c24e8651578d3f14d131
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-08 23:02:01 +0000

    Improve matching

--- src/util/quiz.js
+++ src/util/quiz.js
@@ -3,13 +3,24 @@ const ignoreChars = /[\s,\.:'@~#\]\[\}\{\+=\-_\)\(\*&\^%\$£"!\\`¬<>\?/\|]/gi;
 export function mark(question, guess) {
     guess = normalise(guess);
 
-    if (question.matcher) return question.matcher.test(guess);
+    if (question.matcher) return match(question.matcher, guess);
 
     return equivalent(normalise(question.answer), guess);
 }
 
 function equivalent(left, right) {
     return normalise(left) === normalise(right);
+}
+
+function match(matcher, str) {
+    // returns null or a list of matching substrings
+    const result = str.match(matcher);
+
+    if (result) {
+        return result[0] === str;
+    }
+
+    return false;
 }
 
 function normalise(str) {
