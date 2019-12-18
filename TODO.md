8380141c070e875a8ee2a5d837ac8a82af5f4aa4
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-18 14:37:37 +0000

    Add to TODO

--- TODO.md
+++ TODO.md
@@ -4,6 +4,8 @@ MVP
 * picture round
     * have a picture version of rounds which lays out the picture questions in a grid
 * music round
+    * only one should play at a time
+* sticky headers for round names
 * improve layout of text questions
     * can the input bar be half the width, with the answer put to the right instead of below?
 
@@ -13,3 +15,4 @@ Stretch
 * no spell checking in browser
 * add trivia
     * including the list of all answers when there is a question about how many of something there are
+* don't use whole path for images?
---
+++ src/framework/round-types/PictureListWrapper.svelte
@@ -0,0 +1,29 @@
+<script>
+    import ImagePrompt from "./ImagePrompt.svelte";
+    import Question from "./Question.svelte";
+
+    export let questions;
+    export let reveal;
+    export let roundName;
+    export let score;
+
+    const questionScores = new Array(questions.length).fill(0);
+
+    $: score = questionScores.reduce((tot, score) => tot + score);
+</script>
+
+<style>
+    ol {
+        align-content: flex-start;
+        align-items: flex-start;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        padding: 0;
+    }
+</style>
+
+<ol>
+    <slot/>
+</ol>
