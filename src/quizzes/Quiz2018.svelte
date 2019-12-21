92bd74e1f21b5709b0d89f470f7e20ae91adf601
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-21 17:56:40 +0000

    Correct media paths and make the answers forgiving

--- src/quizzes/Quiz2018.svelte
+++ src/quizzes/Quiz2018.svelte
@@ -15,7 +15,7 @@
             {
                 prompt: "Forza Horizon 4, released in 2018, is set in which country?",
                 answer: "Great Britain",
-                matcher: /greatbritain|gb|(the)?unitedkingdom|uk/
+                matcher: /(great)?britain|gb|(the)?unitedkingdom|uk/
             },
             {
                 prompt: "In which game(s) might you find an \"Aku Aku\" mask?",
@@ -74,6 +74,10 @@
                     "Luke Carlyl",
                     "The Mad Bomber Group",
                 ],
+                matcher: [
+                    /Lizard/,
+                    /MadBomber/,
+                ],
                 outOf: 2,
             },
             {
@@ -87,11 +91,13 @@
             },
             {
                 prompt: "In \"Oddworld: Abe's Oddysee\", Abe is a factory worker and employee of the year at \"Rupture Farms\". What does Abe learn during the opening cutscene that causes him to try to escape the factory?",
-                answer: "He learns that his species is to be turned into food to save the company from falling profits."
+                answer: "He learns that his species is to be turned into food to save the company from falling profits.",
+                matcher: /(food|eat|(kill.*(species|his people))|((species|his people).*kill))/
             },
             {
                 prompt: "In \"Sonic Heroes\", who is the mysterious client who \"Team Chaotix\" rescue from Dr. Eggman's Sky Base?",
                 answer: "Dr. Eggman / Dr. Robotnik",
+                matcher: /.*(eggman|robotnik).*/
             },
             {
                 prompt: "Introduced to the series in \"Tekken 3\", Eddy Gordo uses which style of martial arts?",
@@ -154,7 +160,8 @@
             },
             {
                 prompt: "What is the name of Fortnite's co-op zombie survival game mode?",
-                answer: "Fortnite: Save the World"
+                answer: "Fortnite: Save the World",
+                matcher: /.*save.*world.*/,
             }
         ]}
     />
@@ -164,72 +171,72 @@
         format="picture"
         questions={[
             {
-                picture: "/media/2018/picture/1.png",
+                picture: "media/2018/picture/1.png",
                 answer: "Grim Fandango",
                 half: /.*(grim|fandango).*/,
             },
             {
-                picture: "/media/2018/picture/2.png",
+                picture: "media/2018/picture/2.png",
                 answer: "Hollow Knight",
                 half: /.*(hollow|knight).*/,
             },
             {
-                picture: "/media/2018/picture/3.png",
+                picture: "media/2018/picture/3.png",
                 answer: "Overwatch",
             },
             {
-                picture: "/media/2018/picture/4.png",
+                picture: "media/2018/picture/4.png",
                 answer: "Limbo",
             },
             {
-                picture: "/media/2018/picture/5.png",
+                picture: "media/2018/picture/5.png",
                 answer: "Team Fortress 2",
                 matcher: /team\s*fortress\s*(2|two)/
             },
             {
-                picture: "/media/2018/picture/6.png",
+                picture: "media/2018/picture/6.png",
                 answer: "Zuma",
             },
             {
-                picture: "/media/2018/picture/7.png",
+                picture: "media/2018/picture/7.png",
                 answer: "Pacman",
             },
             {
-                picture: "/media/2018/picture/8.png",
+                picture: "media/2018/picture/8.png",
                 answer: "Space Impact",
                 half: /.*(space).*/,
             },
             {
-                picture: "/media/2018/picture/9.png",
+                picture: "media/2018/picture/9.png",
                 answer: "Hyper Light Drifter",
                 matcher: /Hyper\s*Light\s*Drifter/
             },
             {
-                picture: "/media/2018/picture/10.png",
+                picture: "media/2018/picture/10.png",
                 answer: "Portal 2",
                 matcher: /portal\s*(2|two)/,
                 half: /.*(portal).*/,
             },
             {
-                picture: "/media/2018/picture/Bonus 1.png",
+                picture: "media/2018/picture/Bonus 1.png",
                 answer: "Super Smash Brothers Ultimate",
                 matcher: /.*smash.*ultimate/,
             },
             {
-                picture: "/media/2018/picture/Bonus 2.png",
+                picture: "media/2018/picture/Bonus 2.png",
                 answer: "Player Unknown's: Battlegrounds",
                 half: /.*(PUBG|battlegrounds?)/,
             },
             {
-                picture: "/media/2018/picture/Bonus 3.png",
+                picture: "media/2018/picture/Bonus 3.png",
                 answer: "Cuphead",
             },
             {
-                picture: "/media/2018/picture/Bonus 4.png",
+                picture: "media/2018/picture/Bonus 4.png",
                 answer: "Tetris",
             },
             {
-                picture: "/media/2018/picture/Bonus 5.png",
+                picture: "media/2018/picture/Bonus 5.png",
                 answer: "Mario Kart 8 Deluxe",
                 matcher: /mario\s*[ck]art\s*(8|eight)\s*deluxe/,
                 half: /mario\s*[ck]art\s*(8|eight)\s*/,
@@ -242,47 +249,47 @@
         explanation="For each music clip, name the game that music comes from."
         questions={[
             {
-                audio: "/media/2018/audio/1.wav",
+                audio: "media/2018/audio/1.wav",
                 answer: "Papers Please",
             },
             {
-                audio: "/media/2018/audio/2.wav",
+                audio: "media/2018/audio/2.wav",
                 answer: "Super Metroid",
                 matcher: /.*metroid.*/
             },
             {
-                audio: "/media/2018/audio/3.wav",
+                audio: "media/2018/audio/3.wav",
                 answer: "Toki Tori 2",
                 matcher: /toki\s*tori\s*(2|two)/,
             },
             {
-                audio: "/media/2018/audio/4.wav",
+                audio: "media/2018/audio/4.wav",
                 answer: "Bayonetta",
             },
             {
-                audio: "/media/2018/audio/5.wav",
+                audio: "media/2018/audio/5.wav",
                 answer: "God of War",
             },
             {
-                audio: "/media/2018/audio/6.wav",
+                audio: "media/2018/audio/6.wav",
                 answer: "Shovel Knight",
             },
             {
-                audio: "/media/2018/audio/7.wav",
+                audio: "media/2018/audio/7.wav",
                 answer: ["Luigi's Mansion", "Totaka's Song"],
-                caveat: "You can also earn a point for giving the name of the tune.",
+                caveat: "You can also earn an extra point for giving the name of the tune.",
                 outOf: 2,
             },
             {
-                audio: "/media/2018/audio/8.wav",
+                audio: "media/2018/audio/8.wav",
                 answer: "Sonic Heroes",
             },
             {
-                audio: "/media/2018/audio/9.wav",
+                audio: "media/2018/audio/9.wav",
                 answer: "Undertale",
             },
             {
-                audio: "/media/2018/audio/10.wav",
+                audio: "media/2018/audio/10.wav",
                 answer: "Octopath Traveller",
             },
         ]}
