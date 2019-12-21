b2df89201e42c63fbb4f9cddf336c2bab12cc22a
Thomas Richards <Deskbot@users.noreply.github.com>
2019-12-21 17:53:10 +0000

    Sync the volume of the music players

--- src/framework/format/AudioPrompt.svelte
+++ src/framework/format/AudioPrompt.svelte
@@ -1,13 +1,27 @@
 <script context="module">
+    let allAudios = new Set();
     let current;
 </script>
 
 <script>
+    import { onMount } from "svelte";
+
     export let caveat;
     export let path;
 
     let audio;
     let paused = true;
+
+    onMount(() => {
+        allAudios.add(audio);
+        return () => allAudios.delete(audio);
+    });
+
+    function setOtherVolumes(volume) {
+        for (const otherAudio of allAudios) {
+            otherAudio.volume = volume;
+        }
+    }
 
     // method copied from https://svelte.dev/tutorial/sharing-code
     function stopOthers() {
@@ -33,6 +47,7 @@
 <audio
     controls
     on:play={stopOthers}
+    on:volumechange={event => setOtherVolumes(event.target.volume)}
     src={path}
     bind:paused
     bind:this={audio}
